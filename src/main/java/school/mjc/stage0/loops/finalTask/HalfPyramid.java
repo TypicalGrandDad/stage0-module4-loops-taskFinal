a43f514708027d19b000ad6d9cbba729f03d8ddf
Nikolay <124496633+TypicalGrandDad@users.noreply.github.com>
2023-02-09 21:01:52 +0300

    Update HalfPyramid.java

--- src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
@@ -2,14 +2,14 @@ package school.mjc.stage0.loops.finalTask;
 
 public class HalfPyramid {
     public void printHalfPyramid(int cathetusLength) {
-        for(int x =2; x<=cathetusLength;x++){
+        for(int x =1; x<=cathetusLength;x++){
             for (int y = 1; y<=cathetusLength-x;y++){
                 System.out.print(" ");
             }
-            for (int z = 2; z <=x; z++){
+            for (int z = 2; z <=x+1; z++){
                 System.out.print((z>=10) ?+ z : "*");
             }
-                System.out.println();
+            System.out.println();
 
         }
 
