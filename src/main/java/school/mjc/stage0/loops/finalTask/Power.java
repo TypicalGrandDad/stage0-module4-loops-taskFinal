840caba33ea39a41f5e155ba741881eac2cd5649
Nikolay <124496633+TypicalGrandDad@users.noreply.github.com>
2023-02-09 20:52:58 +0300

    Update Power.java

--- src/main/java/school/mjc/stage0/loops/finalTask/Power.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Power.java
@@ -7,6 +7,7 @@ public class Power {
             oneMoreNumber = oneMoreNumber * numberToPrint;
         }
         numberToPrint = oneMoreNumber;
+        System.out.println(numberToPrint);
 
     }
 
